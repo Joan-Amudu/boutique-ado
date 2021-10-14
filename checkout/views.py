69780db63d30dd5d23530f55a65def5609e00f89
Joan Amudu <joan.baare@gmail.com>
2021-10-14 11:50:29 +0000

    Added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51Jk1qhFzc3HPKNytHVQMa12GfU7kPLzUaU0kL12wWt9Vr61GWRyYFOUQ0AyXYTw1l5dj40LGC94rchh2AlkBeSeu00avOGTE74',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
